751b9e5a11aaa1b4ce5d8fafc30fd1913380e3a7
Jose Antonio Rosales Giron <joseantrosalesprofesional@gmail.com>
2025-06-02 14:57:04 +0200

    Added state change functionality to the "follow" button

--- projects/00-Hello-World-React/src/components/Card-Follow/Card-Follow.jsx
+++ projects/00-Hello-World-React/src/components/Card-Follow/Card-Follow.jsx
@@ -1,4 +1,20 @@
-export function CardFollow({ userPfp, userName, userTag, formatTag = (userTag) => `@${userTag}`, isFollowing }) {
+import { useState } from "react";
+
+export function CardFollow({
+  userPfp,
+  userName,
+  userTag,
+  formatTag = (userTag) => `@${userTag}`,
+  isFollowing, // Sigue estando definido por si necesito usar el prop para algo
+}) {
+  const [isFollowingState, setIsFollowingState] = useState("follow");
+
+  const toggleFollow = () => {
+    setIsFollowingState((prev) =>
+      prev === "unfollow" ? "follow" : "unfollow"
+    );
+  };
+
   return (
     <article className="tw-follow-card">
       <img className="tw-follow-card-pfp" src={userPfp} alt="pfp" />
@@ -9,7 +25,9 @@ export function CardFollow({ userPfp, userName, userTag, formatTag = (userTag) =
             <span className="tw-follow-card-usertag">{formatTag(userTag)}</span>
           </div>
         </div>
-        <button className="tw-follow-card-button">{isFollowing}</button>
+        <button className="tw-follow-card-button" onClick={toggleFollow}>
+          {isFollowingState === "follow" ? "Follow" : "Unfollow"}
+        </button>
       </header>
     </article>
   );
